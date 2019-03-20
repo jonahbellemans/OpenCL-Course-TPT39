5b99e26eb6ee60b2919c29921da289fe14bb01b3
Jonah Bellemans <jonah.bellemans@student.kuleuven.be>
2019-03-20 10:16:11 +0100

    fixed work dimensions

--- GPU/vector_add/matrix_mult.cpp
+++ GPU/vector_add/matrix_mult.cpp
@@ -278,8 +278,8 @@ clGetPlatformIDs(1, &platform, NULL);
     status = clSetKernelArg(kernel, argi++, sizeof(cl_mem), &output_buf);
     checkError(status, "Failed to set argument 4");
 
-    const size_t global_work_size = [N, N];
-    status = clEnqueueNDRangeKernel(queue, kernel, 1, NULL,
+    const size_t[2] global_work_size = {N, N};
+    status = clEnqueueNDRangeKernel(queue, kernel, 2, NULL,
         &global_work_size, NULL, 2, write_event, &kernel_event);
     checkError(status, "Failed to launch kernel");
 
